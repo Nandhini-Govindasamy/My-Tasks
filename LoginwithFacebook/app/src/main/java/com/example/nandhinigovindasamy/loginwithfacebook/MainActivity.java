401b621f25b59553712a7e5b03ae9ea51cd376dd
Nandhini Govindasamy <nandinigovi143@gmail.com>
2018-08-21 17:00:20 +0530

    get facebook data

--- LoginwithFacebook/app/src/main/java/com/example/nandhinigovindasamy/loginwithfacebook/MainActivity.java
+++ LoginwithFacebook/app/src/main/java/com/example/nandhinigovindasamy/loginwithfacebook/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.nandhinigovindasamy.loginwithfacebook;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 textView.setText("Login Success \n"+
                 loginResult.getAccessToken().getUserId()+"\n"+loginResult.getAccessToken().getToken());
+                setFacebookData(loginResult);
+
             }
 
             @Override
@@ -52,5 +62,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    private void setFacebookData(final LoginResult loginResult)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(
+                loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // Application code
+                        try {
+                            Log.i("Response",response.toString());
+
+                            //String email = response.getJSONObject().getString("email");
+                            String firstName = response.getJSONObject().getString("first_name");
+                            String lastName = response.getJSONObject().getString("last_name");
+                            String middleName = response.getJSONObject().getString("middle_name");
+                            String name = response.getJSONObject().getString("name");
+
+
+
+
+                            Profile profile = Profile.getCurrentProfile();
+                            String id = profile.getId();
+                            String link = profile.getLinkUri().toString();
+                            Log.i("Link",link);
+                            if (Profile.getCurrentProfile()!=null)
+                            {
+                                Log.i("Login", "ProfilePic" + Profile.getCurrentProfile().getProfilePictureUri(200, 200));
+                            }
+
+                            //Log.i("Login" + "Email", email);
+                            Log.i("Login"+ "FirstName", firstName);
+                            Log.i("Login" + "LastName", lastName);
+                            Log.i("Login" + "middle name", middleName);
+                            Log.i("Login" + "name", name);
+                            Log.i("Login" + "id", id);
+
+                           // Log.i("Login" + "Gender", gender);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,first_name,middle_name,last_name,name");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
